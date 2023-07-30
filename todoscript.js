7c714f36b78db554003ac6256aeaa5f9ce6694d3
sanjeev7tiwari <123554781+sanjeev7tiwari@users.noreply.github.com>
2023-07-30 20:36:18 +0530

    Update todoscript.js

--- todoscript.js
+++ todoscript.js
@@ -1,8 +1,7 @@
-// todoScript.js
+// todoscript.js
 const submitTodoNode = document.getElementById("submitTodo");
 const userInputNode = document.getElementById("userInput");
 const prioritySelectorNode = document.getElementById("prioritySelector");
-
 const todoListNode = document.getElementById("todo-item");
 
 submitTodoNode.addEventListener("click", function () {
@@ -14,9 +13,13 @@ submitTodoNode.addEventListener("click", function () {
     return;
   }
 
+  var id = Date.now().toString();
+  var completed = false;
   const todo = {
     todoText,
+    id,
     priority,
+    completed,
   };
 
   fetch("/todo", {
@@ -28,19 +31,37 @@ submitTodoNode.addEventListener("click", function () {
   })
     .then(function (response) {
       if (response.status === 200) {
-        return response.json();
+        showTodoInUI(todo);
+        alert("Successfully added todo");
+      } else if (response.status === 401) {
+        window.location.href = "/login";
       } else {
-        throw new Error("something weird happened");
+        alert("Something weird happened");
       }
     })
-    .then(function (todo) {
-      showTodoInUI(todo);
-    })
     .catch(function (error) {
-      alert(error.message);
+      console.error(error);
     });
 });
 
+fetch("/todo-data")
+  .then(function (response) {
+    if (response.status === 200) {
+      return response.json();
+    } else if (response.status === 401) {
+      window.location.href = "/login";
+    } else {
+      throw new Error("Something weird happened");
+    }
+  })
+  .then(function (todos) {
+    todos.forEach(function (todo) {
+      showTodoInUI(todo);
+    });
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
 function showTodoInUI(todo) {
   const todoItemDiv = document.createElement("div");
   const todoTextNode = document.createElement("span");
@@ -50,7 +71,8 @@ function showTodoInUI(todo) {
   checkbox.type = "checkbox";
 
   checkbox.addEventListener("change", function () {
-    updateTodoOnServer(todo.id, { completed: checkbox.checked })
+    const isChecked = checkbox.checked;
+    updateTodoOnServer(todo.id, isChecked)
       .then(function () {
         if (checkbox.checked) {
           todoTextNode.style.textDecoration = "line-through";
@@ -62,13 +84,50 @@ function showTodoInUI(todo) {
         console.error(error);
       });
   });
+  const taskForm = document.getElementById("taskForm");
 
-  todoTextNode.innerText = todo.todoText+"     ";
+  taskForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const taskName = document.getElementById("taskName").value;
+    const taskImages = document.getElementById("taskImages").files;
+  
+    if (!taskName) {
+      alert("Please enter a task name");
+      return;
+    }
+  
+    const formData = new FormData();
+    formData.append("taskName", taskName);
+    for (let i = 0; i < taskImages.length; i++) {
+      formData.append("taskImages", taskImages[i]);
+    }
+  
+    fetch("/add-task", {
+      method: "POST",
+      body: formData,
+    })
+      .then(function (response) {
+        if (response.status === 200) {
+          // Task added successfully, refresh the task list
+          fetchTaskList();
+          alert("Task added successfully");
+        } else if (response.status === 401) {
+          window.location.href = "/login";
+        } else {
+          throw new Error("Something weird happened");
+        }
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  });
+  
+  todoTextNode.innerText = todo.todoText + "     ";
   priorityNode.innerText = todo.priority;
   deleteButton.innerText = "Delete";
 
   deleteButton.addEventListener("click", function () {
-    deleteTodoOnServer(todo.id) 
+    deleteTodoOnServer(todo.id)
       .then(function () {
         todoItemDiv.remove();
       })
@@ -76,6 +135,7 @@ function showTodoInUI(todo) {
         console.error(error);
       });
   });
+
   // Append everything to the todoItemDiv
   todoItemDiv.appendChild(todoTextNode);
   todoItemDiv.appendChild(priorityNode);
@@ -83,55 +143,45 @@ function showTodoInUI(todo) {
   todoItemDiv.appendChild(deleteButton);
 
   todoListNode.appendChild(todoItemDiv);
+  userInputNode.value = "";
 }
 
-fetch("/todo-data")
-  .then(function (response) {
-    if (response.status === 200) {
-      return response.json();
-    } else {
-      throw new Error("something weird happened");
-    }
+function deleteTodoOnServer(todoId) {
+  return fetch(`/delete-todo/${todoId}`, {
+    method: "DELETE",
   })
-  .then(function (todos) {
-    todos.forEach(function (todo) {
-      showTodoInUI(todo);
+    .then(function (response) {
+      if (response.status !== 200) {
+        throw new Error("Failed to delete todo on server");
+      }
+      // Task deleted successfully, remove it from the UI
+      const todoDiv = document.getElementById(todoId);
+      todoDiv.remove();
+    })
+    .catch(function (error) {
+      console.error(error);
     });
-  })
-  .catch(function (error) {
-    console.error(error);
-  });
+}
 
-  function deleteTodoOnServer(todoId) {
-    return fetch(`/delete-todo/${todoId}`, {
-      method: "DELETE",
+
+function updateTodoOnServer(todoId, updates) {
+  const requestBody = { completed: updates };
+
+  return fetch(`/update-todo/${todoId}`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  })
+    .then(function (response) {
+      if (response.status !== 200) {
+        throw new Error("Failed to update todo on server");
+      }
+      return response.json(); // Parse the response JSON data
     })
-      .then(function (response) {
-        if (response.status !== 200) {
-          throw new Error("Failed to delete todo on server");
-        }
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }
-  
-  function updateTodoOnServer(todoId, updates) {
-    return fetch(`/update-todo/${todoId}`, {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(updates),
-    })
-      .then(function (response) {
-        if (response.status !== 200) {
-          throw new Error("Failed to update todo on server");
-        }
-        return response.json(); // Parse the response JSON data
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  }
-  
+    .catch(function (error) {
+      console.error(error);
+    });
+}
+
